2effd9dd3c924c698fdb8b2f16484ed671522540
taewon <tasoho1@naver.com>
2023-03-09 19:26:14 +0900

    final

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from typing import List
 
-
 def twoSum(nums: List[int], target: int) -> List[int]:
 # YOUR ANSWER
     for i in range(1, len(nums)):
         for j in range(i):
             if nums[i] + nums[j] == target:
+                if i > j :
+                    tmp = i
+                    i = j
+                    j = tmp
                 return print([i, j])
 
-twoSum([1,2,5],7)
+# twoSum([1,32,3,9,66,5],33)
